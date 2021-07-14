185ceb9662ac65848de05d5d074abb06a846f643
Lion-Lissauer <84527749+Lion-Lissauer@users.noreply.github.com>
2021-07-14 21:13:34 +0200

    Add files via upload

--- gamelogic.js
+++ gamelogic.js
@@ -1,17 +1,45 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+    const lettersInWordArray = word.split("");
+    let output = "";
+    for (let i = 0; i < lettersInWordArray.length; i++) {
+        const letterInWord = lettersInWordArray[i];
+        const isLetterGuessed = guesses.includes(letterInWord);
+        if (!isLetterGuessed) {
+            output = output + "_ ";
+        }
+        if (isLetterGuessed) {
+            output = output + letterInWord + " ";
+        }
+    }
+    return output;
 }
 
 function isGameWon(word, guesses) {
-  // WRITE ME
+    const lettersInWordArray = word.split("");
+    for (let i = 0; i < lettersInWordArray.length; i++) {
+        const letter = lettersInWordArray[i];
+        const gameWon = guesses.includes(letter)
+        if (!gameWon) {
+            return false
+        }
+    }
+    return true
 }
 
 function isGameLost(word, guesses) {
-  // WRITE ME
+    const lettersInWordArray = word.split("");
+    let wrongLetter = 0;
+    for (let i = 0; i < guesses.length; i++) {
+        let letter = guesses[i];
+        if (!lettersInWordArray.includes(letter)) {
+            wrongLetter++
+        }
+    }
+    return wrongLetter >= 7
 }
 
 module.exports = {
-  displayWordSoFar: displayWordSoFar,
-  isGameWon: isGameWon,
-  isGameLost: isGameLost,
+    displayWordSoFar: displayWordSoFar,
+    isGameWon: isGameWon,
+    isGameLost: isGameLost,
 };
